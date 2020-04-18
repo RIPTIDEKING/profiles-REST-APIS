a079165cb497bee28490f863dd8b5a9c54e3e875
RiptideKing <2018kucp1009@iiitkota.ac.in>
2020-04-19 00:48:41 +0530

    enabled django admin

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -5,22 +5,28 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseU
 class UserProfileManager(BaseUserManager):
     """Manager for user profiles"""
 
-    def create_user(self, email, name, moNo, password=None):
+    def create_user(self, email, name, password=None):
         """create user"""
         if not email:
             raise ValueError('User mush have a email')
 
         email = self.normalize_email(email)
-        user = self.model(email=email, name=name, moNo=moNo)
+        user = self.model(email=email, name=name)
 
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, name, moNo, password):
+    def create_superuser(self, email, name, password):
         """Create Super User"""
 
-        user = create_user(email, name, moNo, password)
+        if not email:
+            raise ValueError('User mush have a email')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+
+        user.set_password(password)
         user.is_staff = True
         user.is_superuser = True
         user.save(using=self.db)
@@ -32,7 +38,6 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     """User Database Model"""
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
-    moNo = models.IntegerField()
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
